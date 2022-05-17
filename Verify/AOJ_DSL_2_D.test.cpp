11730abc20f2e4372c5f358e32f6b34d173c9434
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-17 13:22:39 +0200

    Update AOJ_DSL_2_D.test.cpp

--- Verify/AOJ_DSL_2_D.test.cpp
+++ Verify/AOJ_DSL_2_D.test.cpp
@@ -1,10 +1,7 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_D"
 
-#include <bits/stdc++.h>
-using namespace std;
-
+#include "Template/template.hpp"
 #include "DataStructures/dualsegtree.hpp"
-#include "Utility/fastio.hpp"
 
 int f(int a, int b) {
   if (b != INT32_MAX)
@@ -14,22 +11,21 @@ int f(int a, int b) {
 }
 int e() { return INT32_MAX; }
 
-FastIO io;
 int main() {
   int n, q;
-  io.read(n, q);
+  cin >> n >> q;
 
   DualSegmentTree<int, f, e> seg(n);
   int c, L, R, x;
   while (q--) {
-    io.read(c);
+    cin >> c;
     if (c == 0) {
-      io.read(L, R, x);
+      cin >> L >> R >> x;
       R++;
       seg.update(L, R, x);
     } else {
-      io.read(x);
-      io.write(seg.query(x));
+      cin >> x;
+      cout << seg.query(x) << endl;
     }
   }
   return 0;
