833e73190e6188547e9ed03674478fa079f7d4e0
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-17 16:32:03 +0200

    Added prime factorization, and NT functions

---
+++ Verify/LC_prime_factorization.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/factorize"
+
+#include "../Template/template.hpp"
+#include "../Utility/fastIO.hpp"
+#include "../Math/fast_prime_factorization.hpp"
+
+int main() {
+  int Q;
+  cin >> Q;
+
+  uint64_t X;
+  for (int q = 0; q < Q; ++q) {
+    cin >> X;
+    auto ret = fast_factorize::factorize(X);
+    sort(ret.begin(), ret.end());
+    cout << ret.size();
+    for (auto& x : ret) cout << ' ' << x;
+    cout << '\n';
+  }
+}
--- Verify/LC_static_range_sum.test.cpp
+++ Verify/LC_static_range_sum.test.cpp
@@ -5,15 +5,13 @@
 
 int main() {
   int N, Q;
-
   cin >> N >> Q;
   vector<ll> a(N + 1);
   for (int i = 1; i <= N; ++i) {
     cin >> a[i];
     a[i] += a[i - 1];
   }
-
-  for (int q = 0, l, r; q < Q; ++q) {
+  for (int i = 0, l, r; i < Q; ++i) {
     cin >> l >> r;
     cout << a[r] - a[l] << nl;
   }
