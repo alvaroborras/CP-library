7568f40138ab7250be3faf5e8b88efc6f8c23ece
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-17 17:12:10 +0200

    Add NT functions

---
+++ Verify/LC_partition_function.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/partition_function"
+
+#include "../Template/template.hpp"
+
+#include "../Math/modint.hpp"
+#include "../Math/formal-power-series-friendly-ntt.hpp"
+#include "../Math/partition.hpp"
+
+const int MOD = 998244353;
+using mint = ModInt<MOD>;
+
+int main() {
+  int N;
+  cin >> N;
+  cout << partition<FPS, mint>(N) << endl;
+}
---
+++ Verify/LC_persistent_union_find.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_unionfind"
+
+#include "../Template/template.hpp"
+#include "../Utility/fastIO.hpp"
+#include "../DataStructures/persistent-array.hpp"
+#include "../DataStructures/persistent-union-find.hpp"
+
+int main() {
+  int N, Q;
+  cin >> N >> Q;
+  vector<PersistentUnionFind> uf(Q + 1);
+  uf[0] = PersistentUnionFind(N);
+  for (int i = 1; i <= Q; i++) {
+    int t, k, u, v;
+    cin >> t >> k >> u >> v;
+    ++k;
+    if (t == 0) {
+      uf[i] = uf[k];
+      uf[i].unite(u, v);
+    }
+    else {
+      cout << (uf[k].find(u) == uf[k].find(v)) << "\n";
+    }
+  }
+}
