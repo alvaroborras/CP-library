0aee1dfd5c96499b8babccc10bed0245e3ad944d
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-17 13:14:07 +0200

    Added DualSegTree test

--- Math/primecount.hpp
+++ Math/primecount.hpp
@@ -1,3 +1,4 @@
+#pragma once 
 
 struct PrimeCount {
   const ll N;
---
+++ Verify/AOJ_DSL_2_D.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_D"
+
+#include "DataStructures/dualsegtree.hpp"
+#include "Template/template.hpp"
+#include "Utility/fastio.hpp"
+
+int f(int a, int b) {
+  if (b != INT32_MAX)
+    return b;
+  else
+    return a;
+}
+int e() { return INT32_MAX; }
+
+FastIO io;
+int main() {
+  int n, q;
+  io.read(n, q);
+
+  DualSegmentTree<int, f, e> seg(n);
+  int c, L, R, x;
+  while (q--) {
+    io.read(c);
+    if (c == 0) {
+      io.read(L, R, x);
+      R++;
+      seg.update(L, R, x);
+    } else {
+      io.read(x);
+      io.write(seg.query(x));
+    }
+  }
+  return 0;
+}
