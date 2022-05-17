764ebbf320c5a6d9bf2d8473cd4f94b059998ec7
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-17 12:32:45 +0200

    Initial commit

---
+++ Verify/LC_aplusb.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;
+}
