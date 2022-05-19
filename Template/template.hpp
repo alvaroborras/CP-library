d36347a3ca47d01d0b1ba768abe59eff13cef7a7
Álvaro Borrás <alvaroborrasf@gmail.com>
2022-05-19 09:20:43 +0200

    Update template.hpp

--- Template/template.hpp
+++ Template/template.hpp
@@ -1,35 +1,9 @@
-#include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
-#include <cassert>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <set>
-#include <bitset>
-#include <string>
-#include <algorithm>
-#include <utility>
-#include <complex>
-#include <array>
-#include <random>
-#include <climits>
-#include <unordered_set>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define rep(x, s, t) for (ll x = (s); (x) <= (t); (x)++)
-#define per(x, s, t) for (ll x = (s); (x) >= (t); (x)--)
 #define reps(x, s) for (ll x = 0; (x) < (ll)(s).size(); (x)++)
 #define chmin(x, y) (x) = min((x), (y))
 #define chmax(x, y) (x) = max((x), (y))
-#define sz(x) ((ll)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define outl(...) dump_func(__VA_ARGS__)
@@ -46,66 +20,108 @@ const double PI = 3.1415926535897932384626433;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<ll, ll> P;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pair<int, int>> vpii;
 
 const int mod = 1e9 + 7;
 
-template< typename T1, typename T2 >
-ostream& operator<<(ostream& os, const pair< T1, T2 >& p) {
-  os << p.first << " " << p.second;
+template <class S, class T>
+pair<S, T>& operator+=(pair<S, T>& s, const pair<S, T>& t) {
+  s.first += t.first, s.second += t.second;
+  return s;
+}
+template <class S, class T>
+pair<S, T>& operator-=(pair<S, T>& s, const pair<S, T>& t) {
+  s.first -= t.first, s.second -= t.second;
+  return s;
+}
+template <class S, class T>
+pair<S, T> operator+(const pair<S, T>& s, const pair<S, T>& t) {
+  return pair<S, T>(s.first + t.first, s.second + t.second);
+}
+template <class S, class T>
+pair<S, T> operator-(const pair<S, T>& s, const pair<S, T>& t) {
+  return pair<S, T>(s.first - t.first, s.second - t.second);
+}
+template <class T> T dot(const pair<T, T>& s, const pair<T, T>& t) {
+  return s.first * t.first + s.second * t.second;
+}
+template <class T> T cross(const pair<T, T>& s, const pair<T, T>& t) {
+  return s.first * t.second - s.second * t.first;
+}
+
+template <typename T> ostream& operator<<(ostream& os, vector<T>& vec) {
+  reps(i, vec) os << vec[i] << " ";
   return os;
 }
-
-template< typename T1, typename T2 >
-istream& operator>>(istream& is, pair< T1, T2 >& p) {
-  is >> p.first >> p.second;
-  return is;
-}
-
-template< typename T >
-ostream& operator<<(ostream& os, const vector< T >& v) {
-  for (size_t i = 0; i < v.size(); i++) {
-    os << v[i] << (i + 1 != v.size()?" ":"");
-  }
+template <typename T> ostream& operator<<(ostream& os, const vector<T>& vec) {
+  reps(i, vec) os << vec[i] << " ";
   return os;
 }
-
-template< typename T >
-istream& operator>>(istream& is, vector< T >& v) {
-  for (T& in : v) is >> in;
-  return is;
+template <typename T> ostream& operator<<(ostream& os, list<T>& ls) {
+  for (auto x : ls)
+    os << x << " ";
+  return os;
 }
-
-template< typename T = int64_t >
-vector< T > make_v(size_t a) {
-  return vector< T >(a);
+template <typename T> ostream& operator<<(ostream& os, const list<T>& ls) {
+  for (auto x : ls)
+    os << x << " ";
+  return os;
 }
-
-template< typename T, typename... Ts >
-auto make_v(size_t a, Ts... ts) {
-  return vector< decltype(make_v< T >(ts...)) >(a, make_v< T >(ts...));
+template <typename T> ostream& operator<<(ostream& os, deque<T>& deq) {
+  reps(i, deq) os << deq[i] << " ";
+  return os;
 }
-
-template< typename T, typename V >
-typename enable_if< is_class< T >::value == 0 >::type fill_v(T& t, const V& v) {
-  t = v;
+template <typename T, typename U>
+ostream& operator<<(ostream& os, pair<T, U>& ope) {
+  os << "(" << ope.first << ", " << ope.second << ")";
+  return os;
 }
-
-template< typename T, typename V >
-typename enable_if< is_class< T >::value != 0 >::type fill_v(T& t, const V& v) {
-  for (auto& e : t) fill_v(e, v);
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& ope) {
+  os << "(" << ope.first << ", " << ope.second << ")";
+  return os;
 }
-
-template< typename F >
-struct FixPoint : F {
-  explicit FixPoint(F&& f) : F(forward< F >(f)) {}
-
-  template< typename... Args >
-  decltype(auto) operator()(Args &&... args) const {
-    return F::operator()(*this, forward< Args >(args)...);
+template <typename T, typename U>
+ostream& operator<<(ostream& os, map<T, U>& ope) {
+  for (auto p : ope)
+    os << "(" << p.first << ", " << p.second << "),";
+  return os;
+}
+template <typename T> ostream& operator<<(ostream& os, set<T>& ope) {
+  for (auto x : ope)
+    os << x << " ";
+  return os;
+}
+template <typename T> ostream& operator<<(ostream& os, multiset<T>& ope) {
+  for (auto x : ope)
+    os << x << " ";
+  return os;
+}
+template <typename T> void outa(T a[], ll s, ll t) {
+  rep(i, s, t) {
+    cout << a[i];
+    if (i < t)
+      cout << " ";
   }
-};
-
-template< typename F >
-inline decltype(auto) MFP(F&& f) {
-  return FixPoint< F >{forward< F >(f)};
+  cout << nl;
+}
+template <typename T, size_t N>
+ostream& operator<<(ostream& os, array<T, N>& arr) {
+  reps(i, arr) os << arr[i] << " ";
+  return os;
+}
+template <typename T, size_t N>
+ostream& operator<<(ostream& os, const array<T, N>& arr) {
+  reps(i, arr) os << arr[i] << " ";
+  return os;
+}
+void dump_func() { cout << nl; }
+template <class Head, class... Tail>
+void dump_func(Head&& head, Tail &&...tail) {
+  cout << head;
+  if (sizeof...(Tail) > 0)
+    cout << " ";
+  dump_func(std::move(tail)...);
 }
